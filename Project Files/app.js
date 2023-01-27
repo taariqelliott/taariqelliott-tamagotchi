68a485d27b71eb62deecb6137bd305499b3dcaf2
Taariq Elliott <taariq.elliott@yahoo.com>
2023-01-27 12:11:46 -0800

    Restructred Folder

--- Project Files/app.js
+++ Project Files/app.js
@@ -25,18 +25,17 @@ class Tamagotchi {
       this.hunger -= 1;
       this.sleepiness -= 1;
       this.boredom -= 1;
-      console.log(`The current hunger is`, this.hunger, `.`);
-      console.log(`The current age is`, this.age, `.`);
-      console.log(`The current sleepiness is`, this.sleepiness, `.`);
-      console.log(`The current boredom is`, this.boredom, `.`);
+      console.log(`Current hunger is:`, this.hunger,`.`, `Current age is:`, this.age, `.`,`Current sleepiness is:`, this.sleepiness, `.`,`Ccurrent boredom is:`, this.boredom, `.`);
       document.getElementById("hungerNum").innerText = taariq.hunger;
       document.getElementById("boredomNum").innerText = taariq.boredom;
       document.getElementById("sleepNum").innerText = taariq.sleepiness;
       document.getElementById("age-update").innerText = taariq.age;
       if (this.age == 30) {
+        this.name = entry;
         alert("You unlocked a new fit!");
         document.getElementById("riq-gif").src = "./Project Files/gif folder/2.gif";
       } else if (this.age == 66) {
+        this.name = entry;
         alert(`Woah...${entry} unlocked another wardrobe change!`);
         document.getElementById("riq-gif").src = "./Project Files/gif folder/3.gif";
       }
@@ -47,12 +46,12 @@ class Tamagotchi {
         this.boredom <= 0
       ) {
         clearInterval(this.runGame);
-        console.log(this.hunger, this.age, this.boredom, this.sleepiness);
+        console.log(this.hunger, this.boredom, this.sleepiness,this.age);
         console.log("DONE");
         document.getElementById("riq-gif").src = "./Project Files/gif folder/jet.gif";
         document.getElementById(
           "nameEntry"
-        ).innerText = `${entry} went to a new server!!`;
+        ).innerText = `${entry} went to a new server! Game Over!`;
       }
     }, 1000);
   }
@@ -71,10 +70,7 @@ class Tamagotchi {
 }
 
 const taariq = new Tamagotchi("Taariq");
-
 taariq.name = entry;
-
-console.log(taariq);
 
 // FUNCTIONS THAT WILL RUN FUNCTIONS AND  UPDATE THE RESPECTIVE NUMBER ELEMENT
 const updateHunger = () => {
@@ -116,18 +112,21 @@ function lightsOut() {
   alert("It's about to get dark!");
 }
 
-// DECLARING VARIABLES THAT SELECT THE BUTTONS
+// DECLARING VARIABLES THAT SELECT THE BUTTONS////////////////////////////////////////////////////
+
 let hungerBtn = document.querySelector("#hunger-button");
 let boredBtn = document.querySelector("#boredom-button");
 let sleepBtn = document.querySelector("#sleep-button");
 let lightsOff = document.querySelector("#suprise-button");
 let boxes = document.querySelectorAll(".move-us");
 
-// ADDING EVENT LISTENERS TO THE BUTTONS WHEN CLICKED
+
+// ADDING EVENT LISTENERS TO THE BUTTONS WHEN CLICKED////////////////////////////////////////////
+
 hungerBtn.addEventListener("click", updateHunger);
 boredBtn.addEventListener("click", updateBoredeom);
 sleepBtn.addEventListener("click", updateSleep);
 lightsOff.addEventListener("click", lightsOut);
 
-let el = document.getElementById("riq-gif"); // get a reference to the targeted element
-el.classList.add("riq-gif"); // add the class name to that element
+// let el = document.getElementById("riq-gif"); // get a reference to the targeted element
+// el.classList.add("riq-gif"); // add the class name to that element
